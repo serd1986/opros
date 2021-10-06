af54783e50122994bebe49f80ff1c0b7f4e3b246
Sergey Egorov <serd1986@mail.ru>
2021-10-06 13:31:39 +0400

    fix error

--- src/main/java/com/processdev/model/Rayon.java
+++ src/main/java/com/processdev/model/Rayon.java
@@ -1,7 +1,24 @@
 package com.processdev.model;
 
 public enum Rayon {
-    Центральный,
-    Железнодорожный,
-    Ленинский
+//    Центральный,
+//    Железнодорожный,
+//    Ленинский
+
+    ULIYN_RN("Ульяновский р-н"),
+    CILN_RN("Цильнинский р-н"),
+    NOVO_ULIAN("г.Новоульяновск");
+
+    private String value;
+
+  Rayon(String value){
+        this.value= value;
+  }
+    public String getValue(){
+          return value;
+    }
+    @Override
+    public String toString() {
+        return this.toString();
+    }
 }
